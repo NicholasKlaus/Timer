795720d0774b7d96edce589141b602920767b565
NicholasKlaus <gumenukmikola@gmail.com>
2021-07-23 12:14:01 +0300

    edited actions, types, timerReducer

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,14 +1,21 @@
 import { TYPES } from './constants/types';
 
+export const incSession = () => ({type: TYPES.INC_SESSION})
 
-export const decreaseCounter = () => {
-  return {
-    type: TYPES.DECREMENT
-  }
-}
+export const decSession = () => ({type: TYPES.DEC_SESSION})
 
-export const increaseCounter = () => {
-  return {
-    type: TYPES.INCREMENT
-  }
-}
+export const incBreak = () => ({type: TYPES.INC_BREAK})
+
+export const decBreak = () => ({type: TYPES.DEC_BREAK})
+
+export const decSeconds = () => ({type: TYPES.DEC_SECONDS})
+
+export const switchSession = () => ({type: TYPES.SWITCH_SESSION})
+
+export const switchBreak = () => ({type: TYPES.SWITCH_BREAK})
+
+export const startTimer = () => ({type: TYPES.START_TIMER})
+
+export const stopTimer = () => ({type: TYPES.STOP_TIMER})
+
+export const resetTimer = () => ({type: TYPES.RESET_TIMER})
--- src/redux/timer/timerReducer.ts
+++ src/redux/timer/timerReducer.ts
@@ -3,20 +3,53 @@ import { TYPES } from '../constants/types';
 const initialState = {
   SessionLength: 25,
   BreackLength: 5,
-  isSession: true,
-  timerMinute: 25,
+  interval: 'Session',
+  secondsLeft: 25 * 60,
   timerSecond: 0,
+  timerRunning: false,
 };
 
 export const timerReducer = (state = initialState, action: any) => {
   switch(action.type) {
-    case TYPES.DECREMENT:
+    case TYPES.INC_SESSION:
       return {
 
       }
-    case TYPES.INCREMENT:
+    case TYPES.DEC_SESSION:
       return {
         
+      }
+    case TYPES.INC_BREAK:
+      return {
+
+      }
+    case TYPES.DEC_BREAK:
+      return {
+
+      }
+    case TYPES.DEC_SECONDS:
+      return {
+
+      }
+    case TYPES.SWITCH_SESSION:
+      return {
+
+      }
+    case TYPES.SWITCH_BREAK:
+      return {
+
+      }
+    case TYPES.START_TIMER:
+      return {
+
+      }
+    case TYPES.STOP_TIMER:
+      return {
+
+      }
+    case TYPES.RESET_TIMER:
+      return {
+
       }
     default:
       return state;
